b3a507874c984563f67df44be05c4747c7d5f437
zeon1224 <jjy0477@gmail.com>
2026-04-16 12:16:10 +0900

    week07: memory management v1.3

--- week07/app01.cpp
+++ week07/app01.cpp
@@ -4,28 +4,32 @@ using namespace std;
 int main() {
 	//int rowColumns[3][5];
 	//int rows = 3;
-	int columns = 0;
-	int* arr[3]; // statck memory
+	int columns = 0, rows =0;
+	int** arr = nullptr;
+	cin >> rows;
 	cin >> columns;
 
-	for (int i = 0; i < 3; i++)
-		arr[i] = new int[columns]; //heap memory
+	arr = new int* [rows]; //힙메모리에 포인터 배열을 동적할당
+	for (int i = 0; i < rows; i++)
+		arr[i] = new int[columns]; //힙메모리에 정수 타입을 담는 배열 동적 할당
 
-	for (int i = 0; i < 3; i++) {
+	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j < columns; j++) {
 			arr[i][j] = i * j;
 		}
 	}
 
-	for (int i = 0; i < 3; i++) {
+	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j < columns; j++) {
 			cout << arr[i][j] << " ";
 		}
 		cout << "\n";
 	}
-		for (int i = 0; i < 3; i++) {
-			delete[] arr[i];
-			arr[i] = nullptr;
-		}
-		return 0;
+
+	for (int i = 0; i < rows; i++) {
+		delete[] arr[i];
+		arr[i] = nullptr;
 	}
+	delete[]arr;
+	return 0;
+}
