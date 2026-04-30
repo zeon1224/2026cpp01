8c6ce522221b511d970fe6f1d03b78b8e9abf3e2
zeon1224 <jjy0477@gmail.com>
2026-04-30 11:30:41 +0900

    week09: string v1.6

--- week09/app01.cpp
+++ week09/app01.cpp
@@ -7,12 +7,20 @@ int main() {
 	//char s1[] = { 'A', 'C', 'E' };
 	//char s1[] = { 'A', 'C', 'E', NULL};
 	//char s1[] = { 'A', 'C', 'E',0};
-	//char s1[] = "ACE";
+	char s1[] = "ACE";
+	//char* ps1 = s1;
+	char* ps1 = &s1[0];
 
-	char s1[] = { 'A', 'C',NULL,'E' };
+	//char s1[] = { 'A', 'C',NULL,'E' };
 	//const char char s1[] = { 'A', 'C',NULL ,'E' }; // read only
 	cout << s1 << '\n';
-	s1[2] = 'D';
+	cout << *ps1 << '\n';
+	cout << ps1 << '\n';
+	cout << *s1 << '\n';
+	cout << s1[1] << '\n';
+	cout << *(ps1 + 1) << '\n';
+	//*(ps1 + 2) = 'Z';
+	ps1[2] = 'Z';
 	cout << s1 << '\n';
 
 	return 0;
