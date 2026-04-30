0fe5b88482c918e3cf3e3cbb7667e19d3d8ba832
zeon1224 <jjy0477@gmail.com>
2026-04-30 11:31:38 +0900

    week09:string v1.7

--- week09/app01.cpp
+++ week09/app01.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
-#include <cstring> //c언어 string.h
+#include <cstring>  // string.h
 using namespace std;
 
 int main() {
-	//char s1[] = { 'A', 'C', 'E', '\0'};
-	//char s1[] = { 'A', 'C', 'E' };
-	//char s1[] = { 'A', 'C', 'E', NULL};
-	//char s1[] = { 'A', 'C', 'E',0};
+	//char s1[] = { 'A', 'C', 'E', '\0' };
+	//char s1[] = { 'A', 'C', 'E'};  // !!!
+	//char s1[] = { 'A', 'C', 'E', NULL };	
+	//char s1[] = { 'A', 'C', 'E', 0 };
 	char s1[] = "ACE";
 	//char* ps1 = s1;
 	char* ps1 = &s1[0];
-
-	//char s1[] = { 'A', 'C',NULL,'E' };
-	//const char char s1[] = { 'A', 'C',NULL ,'E' }; // read only
 	cout << s1 << '\n';
 	cout << *ps1 << '\n';
 	cout << ps1 << '\n';
