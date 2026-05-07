d17c09085f2f8c0bf1ec805c5596c9557bb8741b
zeon1224 <jjy0477@gmail.com>
2026-05-07 12:22:30 +0900

    week10 string v2.7

--- week10/app02.cpp
+++ week10/app02.cpp
@@ -1,35 +1,20 @@
-/**************************************************************
- * strcmp 함수와 strncmp 함수를 사용해서                      *
- * 두 문자열을 비교하는 프로그램                              *
- **************************************************************/
 #include <cstring>
 #include <iostream>
 using namespace std;
 
-int myStrcmp(const char* a, const char* b) {
-	int i = 0;
-	while (a[i] != NULL && b[i] != NULL) {
-		if (a[i] != b[i])
-			if (a[i] - b[i] < 0)
-				return -1;
-			else if (a[i] - b[i] > 0)
-				return 1;
-		i++;
-	}
-	if (a[i] < b[i])
-		return -1;
-	else if (a[i] > b[i])
-		return 1;
-	else return 0;
-}
-
 int main()
 {
-	const char* s1 = "Hi Daelim";
-	const char* s2 = "Hi Daelim";
-	cout << strcmp(s1, s2) << endl;
-	//cont <<strncmp(s1,s2,2) << endl;
-	cout << myStrcmp(s1, s2) << endl;
+	//string s1 = "abc";
+	//string s1("abc");
+	//string s1("abc", 2);
+	string s1(5, 'X');
+	cout << s1 << "/" << s1.size() << "/" << s1.capacity() << endl;
+	s1 = "defghijk";
+	cout << s1 << "/" << s1.size() << "/" << s1.capacity() << endl;
+	s1 = "123456789012345";
+	cout << s1 << "/" << s1.size() << "/" << s1.capacity() << endl;
+	s1 = "12345678901234567";
+	cout << s1 << "/" << s1.size() << "/" << s1.capacity() << endl;
 
 	return 0;
 }
