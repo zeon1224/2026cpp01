41d914f3a1f7371722abe21a653adda9c84a64c4
zeon1224 <jjy0477@gmail.com>
2026-05-07 11:09:35 +0900

    week10 string v2.3

--- week10/app01.cpp
+++ week10/app01.cpp
@@ -5,17 +5,21 @@ using namespace std;
 
 int main()
 {
-    char a[] = "ACE";  // ACB, ACZ
-    char b[] = "ACE";
-    string c = "ACE";
-    string d = "ACE";
+    //char* p = "ACE"; // string literal, error
+    const char* p = "ACE"; //string literral(constant), read only
+    cout << p << '\n';
 
-    cout << strcmp(a, b) << '\n';  // 0(두 문자열이 같을때) /  -1, 1(두 문자열이 다를 때)
-    cout << (c == d) << '\n';  // 1(true), 0(false)
+    char a[] = "Hi"; //compact initializer, char a[] = {'H', 'i', '\0'};
+    const char b[] = "Hi"; // compact initializer
+    const char* c = "Hi"; // string literal
 
-    //a = "XYZ";
-    c = "XYZ!";
+    a[1] = 'e';
+    cout << a << '\n';
+    //b[1] = 'e';
+    //cout << b << '\n';
     cout << c << '\n';
+    cout << c[1] << '\n';
+    //c[1] = 'e';
 
     return 0;
 }
